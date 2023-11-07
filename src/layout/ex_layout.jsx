67614150016b62ccc936a69aeedf30ceff69723e
Muhammad Bilal Khan <lofi.audit@gmail.com>
2023-11-07 15:43:35 -0500

    Added layouts, pages w/examples.

---
+++ src/layout/ex_layout.jsx
@@ -0,0 +1,16 @@
+/**
+ * @fileoverview Muhammad Bilal Khan
+ * @class ex_layout
+ * 
+ * 
+ * Layouts are typically used to formulate the ideal look and feel of an application,
+ * by pulling together common components into a hierarchy. 
+ * 
+ * For example:
+ * <NavBar />
+ * <SideBar />
+ * <Content />
+ * <Footer />
+ * 
+ * The layout folder is used to store all the layouts of the application. Please use it as such.
+ */
---
+++ src/pages/ex_pages.jsx
@@ -0,0 +1,14 @@
+/**
+ * @fileoverview Muhammad Bilal Khan
+ * @class ex_pages
+ * 
+ * Pages are typically used to formulate the ideal routing of an application,
+ * by situating css, jsx, and js files into a folder that is named after the page.
+ * 
+ * For example:
+ * ./pages/home/home.jsx
+ * ./pages/home/home.css
+ * ./pages/home/home.test.js
+ * 
+ * The pages folder is used to store all the pages of the application. Please use it as such, or configure it to your liking.
+ */
