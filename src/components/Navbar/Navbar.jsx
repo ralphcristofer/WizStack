50fea77695e05ae5362e75e719358e9cd0a85ecf
Dongli Liu <dongliliu0@gmail.com>
2023-12-04 17:01:07 -0800

    set layout, demo userContext, visual part of Navbar

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,7 +1,10 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import './navbar.module.css';
 import Logo from '../../assets/Wizstack Title.png'
 import { Flex, Text, Button, Link, Box } from '@radix-ui/themes';
+import { UserContext } from '../../contexts/UserContext.jsx';
+
+
 
 const navbarStyle = {
     position: 'fixed',
@@ -13,14 +16,18 @@ const navbarStyle = {
     boxShadow: '0 4px 8px rgba(0, 0, 0, 0.1)',
   };
 
+const logoStyle = {
+    height: "50px",
+}
+
 const Navbar = () => { 
+    const {User} = useContext(UserContext);
+
     return (
         <Flex direction="row" justify="between" align="center" style={navbarStyle}> 
-            <Box>
-                <Link size="2">
-                    <img src={Logo} alt="WizStack Title" height={50} />
-                </Link>
-            </Box>
+            <Link size="2">
+                <img src={Logo} alt="WizStack Title" style={logoStyle} />
+            </Link>
             <Flex gap="5">
                 <Link to={'/'}>Dashboard</Link>
                 <Link to={'/'}>About</Link>
