690dd7f0bd63bf98ff162c4134423a3abb620018
Jenny Zhang <jenny.zhang870305@gmail.com>
2023-12-04 21:54:54 -0500

    Add Login Component

--- src/App.jsx
+++ src/App.jsx
@@ -1,46 +1,61 @@
-import './App.css';
-import '@radix-ui/themes/styles.css';
-import { Theme } from '@radix-ui/themes';
-import { createBrowserRouter, Route, createRoutesFromElements, Link, Outlet, RouterProvider } from 'react-router-dom';
-import { Home } from './pages/Home/index.jsx';
-import { About } from './pages/About/index.jsx';
-import { Contact } from './pages/Contact/index.jsx';
+import "./App.css";
+import "@radix-ui/themes/styles.css";
+import { Theme } from "@radix-ui/themes";
+import {
+  createBrowserRouter,
+  Route,
+  createRoutesFromElements,
+  Link,
+  Outlet,
+  RouterProvider,
+} from "react-router-dom";
+import { Home } from "./pages/Home/index.jsx";
+import { About } from "./pages/About/index.jsx";
+import { Contact } from "./pages/Contact/index.jsx";
+import LoginForm from "./components/Login/Login.jsx";
 
 const App = () => {
   const router = createBrowserRouter(
-     createRoutesFromElements(
-        <Route path="/" element={<Root />}>
-          <Route index element={<Home />} />
-          <Route path="/about" element={<About />} />
-          <Route path="/contact" element={<Contact />} />
-        </Route>
-     )
-  )
+    createRoutesFromElements(
+      <Route path="/" element={<Root />}>
+        <Route index element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/login" element={<LoginForm />}></Route>
+      </Route>
+    )
+  );
 
   return (
-    <Theme appearance="dark" accentColor="tomato" grayColor="mauve" radius="large">
-      <div className='App'>
-        <RouterProvider router={router}/>
+    <Theme
+      appearance="dark"
+      accentColor="tomato"
+      grayColor="mauve"
+      radius="large"
+    >
+      <div className="App">
+        <RouterProvider router={router} />
       </div>
     </Theme>
-  )
-}
+  );
+};
 
 const Root = () => {
-    return (
-      <>
-        <div>
-          <Link to="/">Home</Link>
-          <Link to="/about">About</Link>
-          <Link to="/contact">Contact</Link>
-          <Link to="/product">Product</Link>
-        </div>
+  return (
+    <>
+      <div>
+        <Link to="/">Home</Link>
+        <Link to="/about">About</Link>
+        <Link to="/contact">Contact</Link>
+        <Link to="/product">Product</Link>
+      </div>
 
-        <div>
-          <Outlet />  
-        </div> 
-      </>
-    )
-}
+      <div>
+        <Outlet />
+        <LoginForm />
+      </div>
+    </>
+  );
+};
 
 export default App;
