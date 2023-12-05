0a4d0adc36ccef3e4cacf5085cac96423b4fdfd6
Muhammad Bilal Khan <lofi.audit@gmail.com>
2023-12-04 23:17:13 -0500

    change: First stage change for home page, removed tailwindcss

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,7 +1,32 @@
+import Navbar from "../../components/Navbar/Navbar";
+import Footer from "../../components/Footer/Footer";
+import { Button } from "@radix-ui/themes";
+import "./home.module.css";
+
 export const Home = () => {
     return (
-        <div>
-            <h1>Home Route Yess!!</h1>
-        </div>
-    )
+        <>
+            <Navbar />
+            <section className="home__hero">
+                <h1>Welcome to Wizstack</h1>
+                <p>A solution for students who are looking to organize their studying workflow.</p>
+                <Button variant="primary">Get Started</Button>
+            </section>
+
+            <section className="home__about">
+                <h2>What is Wizstack?</h2>
+            </section>
+
+            <section className="home__product">
+                <h2>Using our Dashboard</h2>
+                <p>
+                    As a student, using our dashboard will help you organize your studying workflow.
+                    You are able to keep track of your assignments, check your schedule using a calendar,
+                    and shuffle through any course materials you have uploaded.
+                </p>
+                <Button variant="primary">Learn More</Button> 
+            </section>
+            <Footer />
+        </>
+    );
 }
