d824fbd103d040251dcf22a03cf115d8dc8de076
Dongli Liu <dongliliu0@gmail.com>
2023-12-09 22:25:09 -0800

    discarded Redix style, split style into css

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,63 +1,47 @@
 import React, { useContext } from 'react';
-import './navbar.module.css';
+import styles from '../../components/Navbar/navbar.module.css';
 import Logo from '../../assets/Wizstack Title.png'
-import { Flex } from '@radix-ui/themes';
 import { useUser } from '../../contexts/UserContext.jsx';
 import { Link } from 'react-router-dom';
 
-const navbarStyle = {
-    top: 0,
-    left: 0,
-    width: '100%',
-    padding: '15px',
-    background: 'var(--gray-a2)', 
-    boxShadow: '0 4px 8px rgba(0, 0, 0, 0.1)',
-  };
-
-const logoStyle = {
-    height: "50px",
-}
-
-const Navbar = () => { 
-    // some variables from the context 
-    const {user, signout} = useUser();
-    const isAdmin = user?.role === 'admin';
+const Navbar = () => {
+    // Variables from the context
+    const { user, signout } = useUser();
+    const isAdmin = user?.role === 'administrator';
     const isAuthenticated = !!user;
 
     return (
-        <Flex direction="row" justify="between" align="center" style={navbarStyle}> 
-            <Link size="2">
-                <img src={Logo} alt="WizStack Title" style={logoStyle} />
+        <div className={styles.navbar_container}>
+            {/* Logo linking to the home page */}
+            <Link to={'/'}>
+                <img src={Logo} alt="WizStack Title" className={styles.logo} />
             </Link>
-            <Flex gap="5">
+            {/* Navigation links */}
+            <div className={styles.navbar_links}>
                 <Link to={'/dashboard'}>DASHBOARD</Link>
                 <Link to={'/about'}>ABOUT</Link>
                 <Link to={'/contact'}>CONTACT</Link>
                 <Link to={'/'}>HOME</Link>
-            </Flex>
-            <Flex gap="3"> 
-                {isAdmin && 
-                    <>
-                    <Link to={'/users'}>USERS</Link>
-                    <div>|</div>
-                    </>
-                }
+            </div>
+            {/* Action links based on user authentication and role */}
+            <div className={styles.navbar_actions}>
+                {isAdmin && <Link to={'/users'}>USERS</Link>}
                 {isAuthenticated ? (
                     <>
-                    <Link to={'/profile'}>PROFILE</Link>
-                    <div>|</div>
-                    <Link to={'/'} onClick={signout}>SIGN OUT</Link>
+                        <Link to={'/profile'}>PROFILE</Link>
+                        <Link to={'/'} onClick={signout}>
+                            SIGN OUT
+                        </Link>
                     </>
                 ) : (
                     <>
-                    <Link to={'/signup'}>SIGN UP</Link>
-                    <div>|</div>
-                    <Link to={'/signin'}>SIGN IN</Link>
+                        <Link to={'/signup'}>SIGN UP</Link>
+                        <Link to={'/signin'}>SIGN IN</Link>
                     </>
                 )}
-            </Flex>
-       </Flex>
+            </div>
+        </div>
     );
-}
+};
 
 export default Navbar;
