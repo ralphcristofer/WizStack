a4cf36456fec22e0471f089abfb783d53ed3106a
Muhammad Bilal Khan <lofi.audit@gmail.com>
2023-12-11 23:15:24 -0500

    update: Base Implementation for About page

--- src/pages/About/index.jsx
+++ src/pages/About/index.jsx
@@ -9,15 +9,107 @@ export const About = () => {
     return (
         <div>
             <Navbar />
+            { /* About Section (IMPLEMENT SOON) */ }
+
+            {/* Services & Features Section */}
             <section className='bg-gray-100'>
-                
+                <div className='container relative flex flex-col justify-between h-full max-w-6xl px-10 mx-auto xl:px-0 mt-5'>
+                    <h2 class="mb-1 text-3xl font-extrabold leading-tight text-gray-900">Services & Features</h2>
+                    <p class="mb-12 text-lg text-gray-500">Here are some Services and Features that we provide!</p>
+                    <div class="w-full">
+                        <div class="flex flex-col w-full mb-10 sm:flex-row">
+                            <div class="w-full mb-10 sm:mb-0 sm:w-1/2">
+                                <div class="relative h-full ml-0 mr-0 sm:mr-10">
+                                    <span class="absolute top-0 left-0 w-full h-full mt-1 ml-1 bg-orange-500  rounded-lg"></span>
+                                    <div class="relative h-full p-5 bg-gray-100 border-2 border-orange-500 rounded-lg">
+                                        <div class="flex items-center -mt-1">
+                                            <h3 class="my-2 ml-3 text-lg font-bold text-gray-800">Course List</h3>
+                                        </div>
+                                        <p class="mt-3 mb-1 text-xs font-medium text-orange-400 uppercase">------------</p>
+                                        <p class="mb-2 text-gray-600">
+                                            We provide a course API that allows you to view all the courses that are available,
+                                            as well as the course details for each specific course.
+                                        </p>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="w-full sm:w-1/2">
+                                <div class="relative h-full ml-0 md:mr-10">
+                                    <span class="absolute top-0 left-0 w-full h-full mt-1 ml-1 bg-orange-500  rounded-lg"></span>
+                                    <div class="relative h-full p-5 bg-gray-100 border-2 border-orange-500 rounded-lg">
+                                        <div class="flex items-center -mt-1">
+                                            <h3 class="my-2 ml-3 text-lg font-bold text-gray-800">Comprehensive Dashboard</h3>
+                                        </div>
+                                        <p class="mt-3 mb-1 text-xs font-medium text-orange-400 uppercase">------------</p>
+                                        <p class="mb-2 text-gray-600">
+                                            We provide a comprehensive dashboard that allows for any student to easily manage their
+                                            daily tasks and assignments, coupled with a timetable and course list to help you stay
+                                            on track.
+                                        </p>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="flex flex-col w-full mb-5 sm:flex-row">
+                            <div class="w-full mb-10 sm:mb-0 sm:w-1/2">
+                                <div class="relative h-full ml-0 mr-0 sm:mr-10">
+                                    <span class="absolute top-0 left-0 w-full h-full mt-1 ml-1 bg-orange-500  rounded-lg"></span>
+                                    <div class="relative h-full p-5 bg-gray-100 border-2 border-orange-500 rounded-lg">
+                                        <div class="flex items-center -mt-1">
+                                            <h3 class="my-2 ml-3 text-lg font-bold text-gray-800">Community</h3>
+                                        </div>
+                                        <p class="mt-3 mb-1 text-xs font-medium text-orange-400 uppercase">------------</p>
+                                        <p class="mb-2 text-gray-600">
+                                            With a strong sense of community, you can be sure that you will be able to find help
+                                            whenever you need it. Talk with students who are in similar situations as you and
+                                            help each other out!
+                                        </p>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="w-full mb-10 sm:mb-0 sm:w-1/2">
+                                <div class="relative h-full ml-0 mr-0 sm:mr-10">
+                                    <span class="absolute top-0 left-0 w-full h-full mt-1 ml-1 bg-orange-500  rounded-lg"></span>
+                                    <div class="relative h-full p-5 bg-gray-100 border-2 border-orange-500 rounded-lg">
+                                        <div class="flex items-center -mt-1">
+                                            <h3 class="my-2 ml-3 text-lg font-bold text-gray-800">Timetable & Calender</h3>
+                                        </div>
+                                        <p class="mt-3 mb-1 text-xs font-medium text-orange-400 uppercase">------------</p>
+                                        <p class="mb-2 text-gray-600">
+                                            Our timetable and calender allows you to easily manage your daily tasks and assignments.
+                                            You can also view your course schedule and see when your next class is, or check out 
+                                            the calender to see when your next assignment is due!
+                                        </p>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="w-full sm:w-1/2">
+                                <div class="relative h-full ml-0 md:mr-10">
+                                    <span class="absolute top-0 left-0 w-full h-full mt-1 ml-1 bg-orange-500  rounded-lg"></span>
+                                    <div class="relative h-full p-5 bg-gray-100 border-2 border-orange-500 rounded-lg">
+                                        <div class="flex items-center -mt-1">
+                                            <h3 class="my-2 ml-3 text-lg font-bold text-gray-800">24/7 Support</h3>
+                                        </div>
+                                        <p class="mt-3 mb-1 text-xs font-medium text-orange-400 uppercase">------------</p>
+                                        <p class="mb-2 text-gray-600">
+                                            Having trouble with something? No worries! We provide 24/7 support to help you with any issue
+                                            that you might have regarding our services. Please not that we are not responsible for any
+                                            issues that you might have with your school or university, or any issues with deadlines 
+                                            that you might miss.
+                                        </p>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
             </section>
 
             { /* Testimonials Section */}
             <section className='bg-gray-100'>
                 <div class="p-4 mx-auto max-w-7xl">
                     <div class="mb-32 text-center">
-                        <h1 class="mb-4 text-3xl font-bold text-black"> Testimonials </h1>
+                        <h1 class="mb-4 text-3xl font-bold text-black">Testimonials</h1>
                         <p class="max-w-xl mx-auto text-gray-500">
                             Hear what other students have to say about our dashboard managment system and how it has
                             helped them to manage their daily tasks.
