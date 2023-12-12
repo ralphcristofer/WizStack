8dd58e56c5fad4722c66961d564dffbb7c43a3f3
ralphcristofer <rcpfugaban@gmail.com>
2023-12-12 10:27:32 -0500

    Changing localhost to render API deployment

--- src/services/axios.js
+++ src/services/axios.js
@@ -1,7 +1,11 @@
 import axios from "axios";
 
+const baseURL = process.env.NODE_ENV === 'production'
+   ? process.env.REACT_APP_API_URL
+   : 'http://localhost:3000';
+
 const axiosInstance = axios.create({
-   baseURL: "http://localhost:3000",
+   baseURL: baseURL,
 });
 
 export default axiosInstance;
